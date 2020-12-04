2b31406bc703a78cfe74ab4596d48642c078907f
Niteshkumar <nitesh156200@gmail.com>
2020-12-04 13:38:49 +0530

    tulip: Remove duplicate basic legacy usb hal
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>

--- device.mk
+++ device.mk
@@ -83,10 +83,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.device.rc
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
-
 # Watermark
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media/MIUI_DualCamera_watermark.png:$(TARGET_COPY_OUT_VENDOR)/etc/MIUI_DualCamera_watermark.png
